5c0d45a69f981f1cab175acfb306550d97898e50
xvoss <xvoss@cs.nmt.edu>
2019-05-25 14:41:20 -0700

    challenge for AES in ECB mode

---
+++ challenge7.py
@@ -0,0 +1,25 @@
+"""
+set 1: AES in EBC mode
+"""
+from Crypto.Cipher import AES
+import base64
+
+def main():
+    KEY = b"YELLOW SUBMARINE"
+
+    ctextb64 = ""
+    with open("data/7.txt", "r") as file1:
+        for line in file1:
+            ctextb64 += line.rstrip()
+
+    ctext = base64.b64decode(ctextb64)
+
+
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    ptext = cipher.decrypt(ctext)
+
+    print("[*] Decrypted text: ")
+    print(ptext.decode('ascii'))
+
+if __name__ == '__main__':
+    main()
---
+++ challenge8.py
@@ -0,0 +1,37 @@
+"""
+set 1: Detect AES in ECB mode
+
+Find a ECB ciphered text amoung other ciphered texts.
+the solution would be to find the ciphertext whose blocks
+repeat themselves the most since ECB encryption typically have repeating
+blocks
+"""
+
+import binascii
+from collections import Counter
+
+def find_ecb_cipher(ctexts, bsize):
+    rank = dict()
+    for c in ctexts:
+        blocks = [c[i:i+bsize] for i in range(0, len(c), bsize)]
+        count = Counter(blocks)
+        top = count.most_common()[0][1]
+        rank[top] = c
+    return rank[max(rank.keys())]
+
+def main():
+    ctexts = []
+    with open("data/8.txt", "r") as file1:
+        for line in file1:
+            ctexts.append(binascii.unhexlify(line.rstrip()))
+
+
+    ecb_text = find_ecb_cipher(ctexts, 16)
+    lnum = ctexts.index(ecb_text)
+
+    print("[*] Likely cipher text in ECB mode: {}".format(ecb_text))
+    print("[*] Found at line {}".format(lnum))
+
+
+if __name__ == '__main__':
+    main()
